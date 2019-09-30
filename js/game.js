f110e3223cd497bc2a625151df4a1c31a9ba4dfc
Korolev Pavel <korolev.vrn@gmail.com>
2019-09-30 19:42:38 +0300

    Добавлено определение firstHitTime

--- js/game.js
+++ js/game.js
@@ -12,9 +12,10 @@ function round() {
   let divSelector = randomDivId();
   $(divSelector).addClass("target");
   $(divSelector).text(hits+1);
-  // TODO: помечать target текущим номером
 
-  // FIXME: тут надо определять при первом клике firstHitTime
+  if (hits === 1) {
+    firstHitTime = getTimestamp();
+  }
 
   if (hits === maxHits) {
     endGame();
