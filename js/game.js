a82b485e12af5adec12f5f40df51276a83c37ba4
Korolev Pavel <korolev.vrn@gmail.com>
2019-09-30 19:33:36 +0300

    Убран текст со старых таргетов

--- js/game.js
+++ js/game.js
@@ -10,6 +10,7 @@ function round() {
 
   let divSelector = randomDivId();
   $(divSelector).addClass("target");
+  $(divSelector).text(hits+1);
   // TODO: помечать target текущим номером
 
   // FIXME: тут надо определять при первом клике firstHitTime
@@ -30,9 +31,12 @@ function endGame() {
 }
 
 function handleClick(event) {
-  // FIXME: убирать текст со старых таргетов. Кажется есть .text?
-  if ($(event.target).hasClass("target")) {
+
+  let target = $(event.target);
+  
+  if ($(target).hasClass("target")) {
     hits = hits + 1;
+    target.text('');
     round();
   }
   // TODO: как-то отмечать если мы промахнулись? См CSS класс .miss
